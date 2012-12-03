d4a0bfd371b6457a2652bb8228d57f090ff14d50
jnoga <joseba.nogales@gmail.com>
2012-12-03 16:10:14 +0100

    Section's CSS improved, Validation system changed

--- javascripts/script.js
+++ javascripts/script.js
@@ -6,15 +6,54 @@ $(document).ready(function() {
    $(".section-link").click(function(e) {
    		goToByScroll($(this).prop('id') + '-section');
 	});
-   
-   $("#contactgukere").validate();
+
+   $("#contactgukere").submit(function() {
+
+   		var valid = true;
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+		if($('#cname').val() == '' || $('#cname').val() == 'Nombre'){
+			console.log("cname fail");
+			$('#cname').attr('class', 'error');
+			valid = false;
+		}
+		else{
+			$('#cname').attr('class', '');
+		}
+
+		var cemail = $('#cemail').val();
+		if(cemail == '' || !emailReg.test(cemail)){
+			console.log("cemail fail");
+			$('#cemail').attr('class', 'error');
+			valid = false;
+		}
+		else{
+			$('#cemail').attr('class', '');
+		}
+
+		if($('#ccomments').val() == ''){
+			console.log("ccomments fail");
+			$('#ccomments').attr('class', 'error');
+			valid = false;
+		}
+		else{
+			$('#ccomments').attr('class', '');
+		}
+		return valid;
+   });
 });
+
+function validateName(){
+	
+}
 
 function goToByScroll(id){
 	$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
 };
 
-
+function changeImage(img){
+	$('#avat').src=img;
+}
 
 
 
